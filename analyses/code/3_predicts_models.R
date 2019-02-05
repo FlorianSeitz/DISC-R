8872eea564aaf956fbe09d4be760284f7f107bcd
FlorianSeitz <32487373+FlorianSeitz@users.noreply.github.com>
2019-02-05 10:54:29 +0100

    V1, 2, and 3 changed to feature1, 2, and 3

--- analyses/code/3_predicts_models.R
+++ analyses/code/3_predicts_models.R
@@ -6,12 +6,12 @@ library(qpcR)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # change this to the folder where this script is lying; works only in RStudio
 source("~/cogscimodels/models/gcm/gcm_unidim.R", chdir = TRUE)
 
-dt <- fread("../../data/processed/categorization_exp_pretest.csv", key = "subj_id")
+dt <- fread("../../data/processed/categorization_exp_pretest.csv", key = "subj_id", colClasses = list("character" = "stim"))
 fitted_parm_gcm <- fread(input = "../../data/processed/categorization_fitted_parm_gcm.csv", key = "subj_id")
 fitted_parm_gcm_unidim <- fread(input = "../../data/processed/categorization_fitted_parm_gcm_unidim.csv", key = "subj_id")
 
 predict_gcm <- function(data, metr, id, unidim = FALSE) {
-  args <- list(formula = response ~ V1 + V2 + V3, 
+  args <- list(formula = response ~ feature1 + feature2 + feature3, 
                data = data[block == "training"], cat = ~ true_cat, metric = metr, 
                choicerule = "soft")
   if (unidim == FALSE) {
@@ -28,35 +28,5 @@ dt[, pred_mink := predict_gcm(data = .SD, metr = "mink", id = unique(subj_id)), 
 dt[, pred_disc_unidim := predict_gcm(data = .SD, metr = "disc", id = unique(subj_id), unidim = TRUE), by = subj_id]
 dt[, pred_mink_unidim := predict_gcm(data = .SD, metr = "mink", id = unique(subj_id), unidim = TRUE), by = subj_id]
 dt[, pred_random := 0.5]
-fwrite(dt, "../../data/processed/categorization_data_with_predictions.csv")
-
-# log likelihoods and AICs
-# column names
-cols <- colnames(dt)[grepl(pattern = "^pred", x = colnames(dt))]
-out_cols <- substring(cols, regexpr("_", cols) + 1)
-
-# Calculate log likelihoods for test set
-ll <- dt[block == "test", lapply(.SD, function(x) {gof(obs = response, pred = x, type = "loglik", response = 'disc', na.rm = TRUE)}), .SDcols = cols, by = subj_id]
-setnames(ll, cols, out_cols)
-
-# Calculate best fitting model for test set
-ll[, best_fitting_model := names(which.max(.SD)), by = subj_id]
-
-# Calculate weights for test set (equal to AIC weights, because no free parameters)
-weights <- ll[, exp(.SD - max(.SD)), by = subj_id, .SDcols = out_cols]
-weights[, (out_cols) := .SD/ rowSums(.SD), by = subj_id]
-
-# Calculate log likelihoods for learning set
-ll <- dt[block == "training", lapply(.SD, function(x) {gof(obs = response, pred = x, type = "loglik", response = 'disc', discount = 8)}), .SDcols = cols, by = subj_id]
-colnames(ll)[colnames(ll) %in% cols] <- out_cols
-
-# Calculate AICs for learning set
-AIC <- ll[, -2 * .SD + 2 * c(5, 5, 2, 2), by = subj_id]
-AIC[, random := dt[block == "training", .(V1 = -2 * gof(obs = response, pred = rep(0.5, length(response)), type = "loglik", response = 'disc')), by = subj_id][, V1]]
-
-
-# To do:
-# 1. Fit parameters for gcm and unidim --> check
-# 2. Log likelihood for testset (gcm and unidim) --> best fitting model, log.likelihood weights / AIC Weights --> check
-# 3. AIC for learningset (gcm) verglcih mit log likelihood von random model (da keine freien Parameter ist AIC = LL); expectation: both gcm versions better than random but equally good
-# 4. deskriptive Resultate plotten (testset predictions aller modelle pro Stimulus (x-Achse) und observations)
+# fwrite(dt, "../../data/processed/categorization_data_with_predictions.csv")
+cat("\n Loaded dt with predictions \n")
