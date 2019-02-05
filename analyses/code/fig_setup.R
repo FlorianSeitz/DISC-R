fc8628e085f5468c6c4c89de09bd30fb549784e3
FlorianSeitz <32487373+FlorianSeitz@users.noreply.github.com>
2019-02-05 10:55:25 +0100

    makes stim_order, xl, and yl

--- analyses/code/fig_setup.R
+++ analyses/code/fig_setup.R
@@ -2,5 +2,10 @@
 library(ggplot2)
 library(Hmisc)
 theme_set(theme_minimal())
-col <- c("MINK" = "red", "MINK_UNIDIM" = "green", "DISC" = "blue", "DISC_UNIDIM" = "burlywood4", "obs" = "lightgrey")
+col <- c("MINK" = "red", "MINK_UNIDIM" = "green", "DISC" = "blue", "DISC_UNIDIM" = "burlywood4", "RANDOM" = "black", "obs" = "lightgrey")
+stim_order <- c("002", "012", "101", "111", "221", "231", "321", "331", "003", "100")
 
+xl <- xlab("Stimulus")
+yl <- ylab("Percentage Cat 1 responses")
+
+cat("\n Loaded col, stim_order, xl, and yl \n")
