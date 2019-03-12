79b8988e6f7eff808b1bf7c2229f9afbdd7f7566
FlorianSeitz <32487373+FlorianSeitz@users.noreply.github.com>
2019-03-12 23:59:07 +0100

    checks whether weights > .90

--- analyses/code/5_compares_models.R
+++ analyses/code/5_compares_models.R
@@ -9,15 +9,18 @@ cols <- colnames(dt)[grepl(pattern = "^pred", x = colnames(dt))]
 out_cols <- substring(cols, regexpr("_", cols) + 1)
 
 # Calculate log likelihoods for test set
-ll_test <- dt[block == "test", lapply(.SD, function(x) {gof(obs = response, pred = x, type = "loglik", response = 'disc', na.rm = TRUE)}), .SDcols = cols, by = subj_id]
+ll_test <- dt[block == "test" & too_slow == FALSE, lapply(.SD, function(x) {gof(obs = response, pred = x, type = "loglik", response = 'disc', na.rm = TRUE)}), .SDcols = cols, by = list(subj_id, time_pressure_cond)]
 setnames(ll_test, cols, out_cols)
 
 # Calculate best fitting model for test set
-ll_test[, best_fitting_model := names(which.max(.SD)), by = subj_id]
+ll_test[, best_fitting_model := names(which.max(.SD)), by = list(subj_id, time_pressure_cond)]
 
 # Calculate weights for test set (equal to AIC weights, because no free parameters)
-weights <- ll_test[, exp(.SD - max(.SD)), by = subj_id, .SDcols = out_cols]
-weights[, (out_cols) := round(.SD/ rowSums(.SD), 2), by = subj_id]
+weights <- ll_test[, exp(.SD - max(.SD)), by = list(subj_id, time_pressure_cond), .SDcols = out_cols]
+weights[, (out_cols) := round(.SD/ rowSums(.SD), 2), by = list(subj_id, time_pressure_cond)]
+
+weights_above_90 <- weights[, base::max(.SD) >= .90, by = list(time_pressure_cond, subj_id)][, V1]
+ll_test[weights_above_90, table(time_pressure_cond, best_fitting_model)]
 
 # Calculate log likelihoods for learning set
 ll_learn <- dt[block == "training", lapply(.SD, function(x) {gof(obs = response, pred = x, type = "loglik", response = 'disc', discount = 8)}), .SDcols = cols, by = subj_id]
