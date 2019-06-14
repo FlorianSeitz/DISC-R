bd1e338191b5eaac926fafdf3a8cc312717136de
FlorianSeitz <32487373+FlorianSeitz@users.noreply.github.com>
2019-06-15 00:33:20 +0200

    makes predictions for familiarization and test phase

--- analyses/code/3_predicts_models.R
+++ analyses/code/3_predicts_models.R
@@ -1,10 +1,14 @@
-# rm(list = ls(all = TRUE))
+rm(list = ls(all = TRUE))
+library(R6)
 library(data.table)
+library(cogscimodels)
 library(cogsciutils)
 library(qpcR)
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # change this to the folder where this script is lying; works only in RStudio
-source("~/cogscimodels/models/gcm/gcm_unidim.R", chdir = TRUE)
+source("~/cogscimodels/R/cogscimodel-class.R", chdir = TRUE)
+source("~/cogscimodels/R/gcm.R", chdir = TRUE)
+source("~/cogscimodels/R/gcm_unidim.R", chdir = TRUE)
 
 dt <- fread("../../data/processed/categorization_exp_main.csv", key = "subj_id", colClasses = list("character" = "stim"))
 fitted_parm_gcm <- fread(input = "../../data/processed/categorization_main_fitted_parm_gcm.csv", key = "subj_id")
@@ -19,7 +23,7 @@ predict_gcm <- function(data, metr, id, unidim = FALSE) {
   } else {
     m <- do.call(gcm_unidim, c(args, fixed = list(unlist(fitted_parm_gcm_unidim[metric == metr & subj_id == id, -(1:2)]))))
   }
-  c(m$predict(), m$predict(newdata = data[block == "test", ]))
+  c(m$predict(), m$predict(newdata = data[block != "training", ]))
 }
 
 # Predict for discrete and Minkowski model
@@ -28,5 +32,5 @@ dt[, pred_mink := predict_gcm(data = .SD, metr = "mink", id = unique(subj_id)), 
 dt[, pred_disc_unidim := predict_gcm(data = .SD, metr = "disc", id = unique(subj_id), unidim = TRUE), by = subj_id]
 dt[, pred_mink_unidim := predict_gcm(data = .SD, metr = "mink", id = unique(subj_id), unidim = TRUE), by = subj_id]
 dt[, pred_random := 0.5]
-# fwrite(dt, "../../data/processed/categorization_data_with_predictions.csv")
+fwrite(dt[, c("subj_id", "trial", grep("^pred", names(dt), value = TRUE)), with = FALSE], "../../data/results/categorization_data_with_predictions.csv")
 cat("\n Loaded dt with predictions \n")
