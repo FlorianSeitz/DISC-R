7023ee6dc348a0e31e3fe13790c2b56495d49f48
FlorianSeitz <32487373+FlorianSeitz@users.noreply.github.com>
2019-06-15 00:32:55 +0200

    sets discount to all except for last 100 learning items

--- analyses/code/2_fits_models.R
+++ analyses/code/2_fits_models.R
@@ -2,7 +2,7 @@ rm(list = ls(all = TRUE))
 library(data.table)
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # change this to the folder where this script is lying; works only in RStudio
-source("~/cogscimodels/models/gcm/gcm_unidim.R", chdir = TRUE)
+source("~/cogscimodels/R/gcm_unidim.R", chdir = TRUE)
 
 dt <- fread("../../data/processed/categorization_exp_main.csv")
 
@@ -10,7 +10,7 @@ dt <- fread("../../data/processed/categorization_exp_main.csv")
 fit_ll <- function(data, metr, id = NULL, unidim = FALSE){
   args <- list(formula = response ~ feature1 + feature2 + feature3, cat = ~ true_cat, metric = metr, fixed = c(r = 1, p = 1), choicerule = "soft")
   if (unidim == FALSE) {
-    m <- do.call(gcm, c(args, data = list(data[block == "training", ]), discount = 8))
+    m <- do.call(gcm, c(args, data = list(data[block == "training", ]), discount = nrow(data)-100))
   } else {
     args$fixed <- c(args$fixed, unlist(fitted_parm_gcm[metric == metr & subj_id == id, c(6, 9)]))
     m <- do.call(gcm_unidim, c(args, data = list(data[!is.na(response), ]), discount = sum(data$block == "training"))) # data = list(data[block == "training", ]), discount = sum(data$block == "training")
@@ -28,7 +28,5 @@ fitted_parm_gcm_unidim <- rbindlist(lapply(c("disc", "mink"), function(x) dt[, {
 fitted_parm_gcm_unidim[, metric := ifelse(metric == 1, "disc", "mink")]
 fwrite(fitted_parm_gcm_unidim, file = "../../data/processed/categorization_main_fitted_parm_gcm_unidim.csv")
 
-# TO DO: 
-# 1. install newest cogscimodels from github
-# 2. discount für multidim: take only last 100 trials 
-# 3. fit with c ranging to 10
+# To Do
+# Fit parameters only to last 100 (as now) and count in N from beginning on
