8292818f61e1bf79b5521702c284365d92e591ec
FlorianSeitz <32487373+FlorianSeitz@users.noreply.github.com>
2019-08-14 09:13:29 +0200

    fit pars only to las 100 and count in N from start

--- analyses/code/2_fits_models.R
+++ analyses/code/2_fits_models.R
@@ -1,12 +1,15 @@
 rm(list = ls(all = TRUE))
 library(data.table)
+library(cogscimodels)
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # change this to the folder where this script is lying; works only in RStudio
 source("~/cogscimodels/R/gcm_unidim.R", chdir = TRUE)
+source("~/cogscimodels/R/gcm.R", chdir = TRUE)
 
-dt <- fread("../../data/processed/categorization_exp_main.csv")
+dt <- fread("../../data/processed/categorization_exp_main.csv",
+            colClasses = list("character" = "stim"))
 
-# Fitting with gcm
+# Fitting learningset with gcm
 fit_ll <- function(data, metr, id = NULL, unidim = FALSE){
   args <- list(formula = response ~ feature1 + feature2 + feature3, cat = ~ true_cat, metric = metr, fixed = c(r = 1, p = 1), choicerule = "soft")
   if (unidim == FALSE) {
@@ -28,5 +31,24 @@ fitted_parm_gcm_unidim <- rbindlist(lapply(c("disc", "mink"), function(x) dt[, {
 fitted_parm_gcm_unidim[, metric := ifelse(metric == 1, "disc", "mink")]
 fwrite(fitted_parm_gcm_unidim, file = "../../data/processed/categorization_main_fitted_parm_gcm_unidim.csv")
 
-# To Do
-# Fit parameters only to last 100 (as now) and count in N from beginning on
+# Fitting testset with gcm
+fit_ll <- function(data, metr, id = NULL, unidim = FALSE){
+  args <- list(formula = response ~ feature1 + feature2 + feature3, cat = ~ true_cat, metric = metr, fixed = c(r = 1, p = 1), choicerule = "soft")
+  if (unidim == FALSE) {
+    m <- do.call(gcm, c(args, data = list(data[!is.na(response), ]), discount = sum(data$block == "training" )))
+  } else {
+    args$fixed <- c(args$fixed, unlist(fitted_parm_gcm_test[metric == metr & subj_id == id, c(6, 9)]))
+    m <- do.call(gcm_unidim, c(args, data = list(data[!is.na(response), ]), discount = sum(data$block == "training")))
+  }
+  return(as.list(c(m$parm)))
+}
+
+# Fit Parameters of discrete and Minkowsi model 
+fitted_parm_gcm_test <- rbindlist(lapply(c("d", "m"), function(x) dt[, {print(.GRP); fit_ll(.SD, metr = x)}, by = subj_id]), id = "metric")
+fitted_parm_gcm_test[, metric := ifelse(metric == 1, "disc", "mink")]
+fwrite(fitted_parm_gcm_test, file = "../../data/processed/categorization_main_fitted_parm_gcm_test.csv")
+
+# Fit parameters to unidim model
+fitted_parm_gcm_unidim_test <- rbindlist(lapply(c("disc", "mink"), function(x) dt[, {print(.GRP); fit_ll(.SD, metr = x, id = unique(subj_id), unidim = TRUE)}, by = subj_id]), id = "metric")
+fitted_parm_gcm_unidim_test[, metric := ifelse(metric == 1, "disc", "mink")]
+fwrite(fitted_parm_gcm_unidim_test, file = "../../data/processed/categorization_main_fitted_parm_gcm_unidim_test.csv")
