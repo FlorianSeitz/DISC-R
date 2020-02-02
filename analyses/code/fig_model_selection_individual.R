54cec622cacf361b807e183b713c579e9533dcaa
FlorianSeitz <32487373+FlorianSeitz@users.noreply.github.com>
2020-02-02 20:21:16 +0100

    implements bw figure

--- analyses/code/fig_model_selection_individual.R
+++ analyses/code/fig_model_selection_individual.R
@@ -3,21 +3,28 @@ library(data.table)
 library(patchwork)
 library(tidyverse)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # change this to the folder where this script is lying; works only in RStudio
-source("fig_setup.R")
 source("5_compares_models.R")
+source("fig_setup.R")
 
 model_distr[, best_fitting_model := factor(best_fitting_model, 
                                            levels = c("disc", "mink", "disc_unidim", "mink_unidim", "random"), 
                                            labels = c("DISC-MULTI", "MINK-MULTI", "DISC-UNI", "MINK-UNI", "RANDOM"))]
+model_distr[, time_pressure_cond := factor(time_pressure_cond, levels = c(TRUE, FALSE))]
 
-labs <- c("Without time pressure", "With time pressure")
-names(labs) <- c("FALSE", "TRUE")
+labs <- c("Time pressure", "No time pressure")
+names(labs) <- c("TRUE", "FALSE")
 
 ggplot(model_distr, aes(x = best_fitting_model, y = N)) +
-  geom_bar(aes(fill = best_fitting_model), stat = "identity") +
+  geom_bar(aes(fill = best_fitting_model), stat = "identity", color = "black") +
+  scale_fill_manual(values = bw_cols) +
   facet_grid(~time_pressure_cond, labeller = labeller(time_pressure_cond = labs)) +
-  ggtitle("Number of people best fit by each model for each time pressure condition") + 
+  # ggtitle("Number of people best fit by each model for each time pressure condition") + 
   theme(plot.title = element_text(hjust = 0.5)) + 
-  theme(legend.position = "none") +
-  xlab("best fitting model")
-ggsave("../../output/images/model_selection_individual.jpg")
+  theme(legend.position = "none",
+        axis.ticks.x = element_blank(),
+        strip.text = element_text(size = 12),
+        axis.title = element_text(size = 12),
+        axis.text = element_text(size = 10)) +
+  xlab("Models") +
+  ylab(expression(paste(italic("N"), " described participants")))
+ggsave("../../output/images/model_selection_individual.png", width = 9.5)
