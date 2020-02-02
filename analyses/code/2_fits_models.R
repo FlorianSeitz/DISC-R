f0c99e3982283c75a1088b89a053141ab654ec12
FlorianSeitz <32487373+FlorianSeitz@users.noreply.github.com>
2020-02-02 20:14:21 +0100

    fits the threshold model

--- analyses/code/2_fits_models.R
+++ analyses/code/2_fits_models.R
@@ -5,6 +5,7 @@ library(cogscimodels)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # change this to the folder where this script is lying; works only in RStudio
 source("~/cogscimodels/R/gcm_unidim.R", chdir = TRUE)
 source("~/cogscimodels/R/gcm.R", chdir = TRUE)
+source("~/cogscimodels/R/cogscimodel-class.R", chdir = TRUE)
 
 dt <- fread("../../data/processed/categorization_exp_main.csv",
             colClasses = list("character" = "stim"))
@@ -35,9 +36,16 @@ fwrite(fitted_parm_gcm_unidim, file = "../../data/processed/categorization_main_
 fit_ll <- function(data, metr, id = NULL, unidim = FALSE){
   args <- list(formula = response ~ feature1 + feature2 + feature3, cat = ~ true_cat, metric = metr, fixed = c(r = 1, p = 1), choicerule = "soft")
   if (unidim == FALSE) {
+    if (grepl("^t", metr)) {
+      args$fixed <- c(args$fixed, gamma = 0)
+    }
     m <- do.call(gcm, c(args, data = list(data[!is.na(response), ]), discount = sum(data$block == "training" )))
   } else {
-    args$fixed <- c(args$fixed, unlist(fitted_parm_gcm_test[metric == metr & subj_id == id, c(6, 9)]))
+    if (grepl("^t", metr)) {
+      args$fixed <- c(args$fixed, unlist(fitted_parm_dtm_test[subj_id == id, c(6, 9, 10)]))
+    } else {
+      args$fixed <- c(args$fixed, unlist(fitted_parm_gcm_test[metric == metr & subj_id == id, c(6, 9)]))
+    }
     m <- do.call(gcm_unidim, c(args, data = list(data[!is.na(response), ]), discount = sum(data$block == "training")))
   }
   return(as.list(c(m$parm)))
@@ -54,4 +62,12 @@ fitted_parm_gcm_unidim_test[, metric := ifelse(metric == 1, "disc", "mink")]
 fwrite(fitted_parm_gcm_unidim_test, file = "../../data/processed/categorization_main_fitted_parm_gcm_unidim_test.csv")
 
 # Fit Parameters of discrete-threshold model 
-fitted_parm_dtm_test <- rbindlist(lapply(c("t"), function(x) dt[subj_id == "mqon", {print(.GRP); fit_ll(.SD, metr = x)}]), id = "metric")
+fitted_parm_dtm_test <- rbindlist(lapply(c("t"), function(x) dt[subj_id == "ysei", {print(.GRP); fit_ll(.SD, metr = x)}, by = subj_id]), id = "metric")
+fitted_parm_dtm_test[, metric := "threshold"]
+fwrite(fitted_parm_dtm_test, file = "../../data/processed/categorization_main_fitted_parm_dtm_test.csv")
+
+# Fit parameters to unidim discrete-threshold model
+fitted_parm_dtm_test <- fread("../../data/processed/categorization_main_fitted_parm_dtm_test.csv")
+fitted_parm_dtm_unidim_test <- rbindlist(lapply(c("t"), function(x) dt[, {print(.GRP); fit_ll(.SD, metr = x, id = unique(subj_id), unidim = TRUE)}, by = subj_id]), id = "metric")
+fitted_parm_dtm_unidim_test[, metric := ifelse(metric == 1, "threshold", NA)]
+fwrite(fitted_parm_dtm_unidim_test, file = "../../data/processed/categorization_main_fitted_parm_dtm_unidim_test.csv")
