d94031a773b1b46eea9598d394a608794eed4c1e
FlorianSeitz <32487373+FlorianSeitz@users.noreply.github.com>
2020-02-02 20:21:47 +0100

    Create source_partial.R

---
+++ analyses/code/source_partial.R
@@ -0,0 +1,5 @@
+source_partial <- function(file, start, end, ...) {
+  file.lines <- scan(file, what=character(), skip=start-1, nlines=end-start+1, sep='\n')
+  file.lines.collapsed <- paste(file.lines, collapse='\n')
+  source(textConnection(file.lines.collapsed), ...)
+}
