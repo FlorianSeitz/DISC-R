06ad7e8bbb515a88899978bfbd3f772b0f78a599
FlorianSeitz <32487373+FlorianSeitz@users.noreply.github.com>
2020-09-05 19:03:16 +0200

    adds three fitting procedures of which one is to choose

--- analyses/code/2_fits_models.R
+++ analyses/code/2_fits_models.R
@@ -1,36 +1,62 @@
 rm(list = ls(all = TRUE))
-library(data.table)
+pacman::p_load(data.table, R6, Rsolnp)
 library(cogscimodels)
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # change this to the folder where this script is lying; works only in RStudio
 source("~/cogscimodels/R/gcm_unidim.R", chdir = TRUE)
 source("~/cogscimodels/R/gcm.R", chdir = TRUE)
 source("~/cogscimodels/R/cogscimodel-class.R", chdir = TRUE)
+devtools::load_all("~/cogscimodels/")
+devtools::load_all("~/cogsciutils/")
 
 dt <- fread("../../data/processed/categorization_exp_main.csv",
             colClasses = list("character" = "stim"))
 
 # Fitting learningset with gcm
+# fit_ll <- function(data, metr, id = NULL, unidim = FALSE){
+#   args <- list(formula = response ~ feature1 + feature2 + feature3, cat = ~ true_cat, metric = metr, fixed = c(r = 1, p = 1), choicerule = "soft")
+#   if (unidim == FALSE) {
+#     m <- do.call(gcm, c(args, data = list(data[block == "training", ])))
+#   } else {
+#     args$fixed <- c(args$fixed, unlist(fitted_parm_gcm[metric == metr & subj_id == id, c("c", "tau")]))
+#     print(args$fixed)
+#     m <- do.call(gcm_unidim, c(args, data = list(data[!is.na(response) & block != "familiarization", ]), discount = sum(data$block == "training")))
+#   }
+#   return(as.list(c(m$parm)))
+# }
+
+# fit_ll <- function(data, metr, id = NULL, unidim = FALSE){
+#   args <- list(formula = response ~ feature1 + feature2 + feature3, cat = ~ true_cat, metric = metr, fixed = c(r = 1, p = 1), choicerule = "soft")
+#   last_trials <- data[, which(which(block == "training") > (sum(block == "training") - 100))]
+#   if (unidim == FALSE) {
+#     m <- do.call(gcm, c(args, data = list(data[block != "test"]), discount = list(last_trials)))
+#   } else {
+#     args$fixed <- c(args$fixed, unlist(fitted_parm_gcm[metric == metr & subj_id == id, c("c", "tau")]))
+#     m <- do.call(gcm_unidim, c(args, data = list(data[!is.na(response) & trial %in% last_trials == FALSE, ]), discount = sum(data$block != "test")))
+#   }
+#   return(as.list(c(m$parm)))
+# }
+
 fit_ll <- function(data, metr, id = NULL, unidim = FALSE){
   args <- list(formula = response ~ feature1 + feature2 + feature3, cat = ~ true_cat, metric = metr, fixed = c(r = 1, p = 1), choicerule = "soft")
   if (unidim == FALSE) {
-    m <- do.call(gcm, c(args, data = list(data[block == "training", ]), discount = nrow(data)-100))
+    m <- do.call(gcm, c(args, data = list(data[block != "test"])))
   } else {
-    args$fixed <- c(args$fixed, unlist(fitted_parm_gcm[metric == metr & subj_id == id, c(6, 9)]))
-    m <- do.call(gcm_unidim, c(args, data = list(data[!is.na(response), ]), discount = sum(data$block == "training"))) # data = list(data[block == "training", ]), discount = sum(data$block == "training")
+    args$fixed <- c(args$fixed, unlist(fitted_parm_gcm[metric == metr & subj_id == id, c("c", "tau")]))
+    m <- do.call(gcm_unidim, c(args, data = list(data[!is.na(response), ]), discount = sum(data$block != "test")))
   }
   return(as.list(c(m$parm)))
 }
 
-# Fit Parameters of discrete and Minkowsi model 
-fitted_parm_gcm <- rbindlist(lapply(c("d", "m"), function(x) dt[block == "training", {print(.GRP); fit_ll(.SD, metr = x)}, by = subj_id]), id = "metric")
+# Fit Parameters of discrete and Minkowski model 
+fitted_parm_gcm <- rbindlist(lapply(c("disc", "mink"), function(x) dt[, {print(.GRP); fit_ll(.SD, metr = x, id = unique(subj_id))}, by = subj_id]), id = "metric")
 fitted_parm_gcm[, metric := ifelse(metric == 1, "disc", "mink")]
-fwrite(fitted_parm_gcm, file = "../../data/processed/categorization_main_fitted_parm_gcm.csv")
+fwrite(fitted_parm_gcm, file = "../../data/processed/categorization_main_fitted_parm_gcm_train3.csv")
 
 # Fit parameters to unidim model
 fitted_parm_gcm_unidim <- rbindlist(lapply(c("disc", "mink"), function(x) dt[, {print(.GRP); fit_ll(.SD, metr = x, id = unique(subj_id), unidim = TRUE)}, by = subj_id]), id = "metric")
 fitted_parm_gcm_unidim[, metric := ifelse(metric == 1, "disc", "mink")]
-fwrite(fitted_parm_gcm_unidim, file = "../../data/processed/categorization_main_fitted_parm_gcm_unidim.csv")
+fwrite(fitted_parm_gcm_unidim, file = "../../data/processed/categorization_main_fitted_parm_gcm_unidim_train3.csv")
 
 # Fitting testset with gcm
 fit_ll <- function(data, metr, id = NULL, unidim = FALSE){
